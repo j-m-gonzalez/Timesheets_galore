3b911ca81cccbd96d71e59a50019f9b915f632d5
j-m-gonzalez <jgonzalez4927@gmail.com>
2021-04-19 15:22:47 -0700

    allowed hosts

--- timesheets_app/settings.py
+++ timesheets_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stage.timesheet.page', 'www.timesheet.page' 'timesheet.page', '161.35.229.31', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['stage.timesheet.page', 'www.timesheet.page', 'timesheet.page', '161.35.229.31', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
